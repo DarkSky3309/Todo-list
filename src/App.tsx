1402c8989702dff12747ea11078b8b5f50dc210d
DarkSky3309 <lust3309@gmail.com>
2023-02-03 13:45:27 +0300

    reducer done, add store

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Heading from "./components/Heading";
+
+function App() {
+    return (
+        <div className="App font-Poppins container py-16 px-6 min-h-screen mx-auto">
+            <Heading/>
+        </div>
+    );
+}
+
+export default App;
